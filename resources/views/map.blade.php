b894a4a43112aecd4e081b3b1174332d4c31901a
dioh0922 <diohound@gmail.com>
2021-07-20 10:13:59 +0900

    スタイルの調整

--- resources/views/map.blade.php
+++ resources/views/map.blade.php
@@ -5,6 +5,7 @@
 		<title>地図表示</title>
 		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
 
+		<link rel="stylesheet" href="resource/css/util.css"/>
 		<link rel="stylesheet" href="resource/css/leaflet.css"/>
 		<script src="resource/js/leaflet.js"></script>
 	</head>
@@ -12,7 +13,7 @@
 
 	<h1>100名城実績ページ</h1>
 
-	<a href="./list"><button class="info-btn">一覧へ</button></a>
+	<a href="./list"><button class="success-btn">一覧へ</button></a>
 	<div id="map" style="width:100%;height:100%;"></div>
 	<link rel="stylesheet" href="resource/css/achieve.css"/>
 	<script src="resource/js/achieve_disp.js"></script>
